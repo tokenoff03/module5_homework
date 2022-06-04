9043f6d7eb930aff051f84a5f89eec188f5df569
Adil <v-i-p.adil@mail.ru>
2022-06-04 17:15:46 +0600

    SeventhTask update

--- seventhTask.js
+++ seventhTask.js
@@ -1,10 +1,13 @@
 let arr = [1,null,3,"abs",5,6,7];
 let countEven = 0;
 let countOdd = 0;
+let zeronumb = 0;
 arr.forEach(value => {
     if(!isNaN(value) && typeof(value) === "number"){
-        if(value%2==0) countEven++;
+        if(value == 0) zeronumb++;
+        else if(value%2==0) countEven++;
         else countOdd++;
     }
+    
 });
-console.log(`Четные: ${ countEven }, Нечетные ${ countOdd }`)
+console.log(`Четные: ${ countEven }, Нечетные: ${ countOdd }, ZeroNumbers ${zeronumb}`)
